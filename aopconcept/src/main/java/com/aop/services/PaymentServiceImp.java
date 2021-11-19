45de3c98f89dcd8f85f50eaf6954b1c2457b287e
Ashish <Ashish@192.168.7.81>
2021-11-19 11:11:56 +0530

    class to define methods and all

---
+++ aopconcept/src/main/java/com/aop/services/PaymentServiceImp.java
@@ -0,0 +1,19 @@
+package com.aop.services;
+
+public class PaymentServiceImp implements PaymentService{
+
+	public void makePayment(int amount) {
+		
+		//payment code
+		
+		System.out.println(amount+" Amount debited...");
+		
+		//
+		//
+		//
+		
+		System.out.println(amount+" Amount credited...");
+		
+	}
+
+}
